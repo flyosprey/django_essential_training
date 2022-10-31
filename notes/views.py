c0b258251d217b5ceb7a8d4a57a48d63409e2ad4
Vlad <vlad.bashtannyk11@gmail.com>
2022-10-31 15:39:11 +0200

    Added template

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+from .models import Notes
 # Create your views here.
+
+
+def notes_list(request):
+    all_notes = Notes.objects.all()
+    return render(request, "notes/notes_list.html", {"notes": all_notes})
