4f827b570e80bd12e88d2d928439ba1a69b418ca
Vlad <vlad.bashtannyk11@gmail.com>
2022-10-31 15:56:23 +0200

    Changed view from func-based into class-based

--- home/views.py
+++ home/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-
-def home(request):
-    return render(request, "home/welcome.html", {"today": datetime.today()})
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-@login_required(login_url="/admin")
-def authorized(request):
-    return render(request, "home/authorized.html", {})
+class HomeView(TemplateView):
+    template_name = "home/welcome.html"
+    extra_context = {"today": datetime.today()}
+
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = "home/authorized.html"
+    login_url = "/admin"
+
 
