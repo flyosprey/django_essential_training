d86b20e14856616e2a746b6e7e175af686383c8d
Vlad <vlad.bashtannyk11@gmail.com>
2022-10-31 15:50:31 +0200

    Added new detail url

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.notes_list)
+    path("notes/", views.notes_list),
+    path("notes/<int:pk>", views.detail),
 ]
