670386ab70708d634d439bff9e3e39429673c4ab
Vlad <vlad.bashtannyk11@gmail.com>
2022-10-31 16:01:30 +0200

    Changed view from func-based into class-based (ListView, DetailView)

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("notes/", views.notes_list),
-    path("notes/<int:pk>", views.detail),
+    path("notes/", views.NotesListView.as_view()),
+    path("notes/<int:pk>", views.NotesDetailView.as_view()),
 ]
